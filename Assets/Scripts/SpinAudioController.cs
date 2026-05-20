eeaf4a145553653834649690ee012d30d819dadc
Saatvik Kundoor <saatvik.kundoor@icloud.com>
2026-05-20 14:31:23 -0500

    Set up Spin Sounds
    Timed Spin sound to spin time and trimmed audio clip accordingly

---
+++ Assets/Scripts/SpinAudioController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinAudioController : MonoBehaviour
+{
+    public AudioClip spinSound;
+    private AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlaySpin()
+    {
+        audioSource.clip = spinSound;
+        audioSource.time = 0f;
+        audioSource.Play();
+        StartCoroutine(StopAfter(3));
+    }
+    IEnumerator StopAfter(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        audioSource.Stop();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
