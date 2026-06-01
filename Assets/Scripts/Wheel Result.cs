beb08e931eb86b23b551acb23f5f700d1ce59dcd
Saatvik Kundoor <saatvik.kundoor@icloud.com>
2026-06-01 00:01:58 -0500

    Set up Answer Checks
    Added logic for converge and Diverge buttons, created answer list with matching tests

--- Assets/Scripts/Wheel Result.cs
+++ Assets/Scripts/Wheel Result.cs
@@ -42,7 +42,7 @@ public class WheelResult : MonoBehaviour
         "1\n-----------\nn(ln(n))",
     
         // Problem 11
-        "(2n)!\n-------------\n5<sup>n</sup>(n!)<sup>2</sup>",
+        "(2n)!\n-----------\n5<sup>n</sup>(n!)<sup>2</sup>",
         
         // Problem 12
         "cos<sup>2</sup>n\n------\nn",
@@ -71,12 +71,82 @@ public class WheelResult : MonoBehaviour
         // Problem 20
         "n<sup>4</sup>+8n\n-----------\n3n<sup>4</sup>+1"
     };
+    string[] answers = new string[]
+    {
+        
+        // Problem 1
+        "Div. by Limit Comparison Test",
+        
+        
+        // Problem 2
+        "Conv. by Geometric Series Test",
+        
+        // Problem 3
+        "Conv. by Integral Test",
+        
+        // Problem 4
+        "Conv. by AST",
+        
+        // Problem 5
+        "Div. by Ratio Test",
+        
+        // Problem 6
+        "Conv. by Limit Comparison Test",
+        
+        // Problem 7
+        "Div. by Limit Comparison Test",
+        
+        // Problem 8
+        "Conv. by Geometric Series Test",
+        
+        // Problem 9
+        "Conv. by Limit Comparison Test",
+
+        //Problem 10
+        "Div. by Integral Test",
+    
+        // Problem 11
+        "Conv. by Ratio Test",
+        
+        // Problem 12
+        "Div. by Direct Comparison Test",
+        
+        // Problem 13
+        "Conv. by Ratio Test",
+        
+        // Problem 14
+        "Conv. by AST",
+        
+        // Problem 15
+        "Div. by Limit Comparison Test",
+        
+        // Problem 16
+        "Conv. by Ratio Test",
+        
+        // Problem 17
+        "Conv. by Limit Comparison Test",
+
+        // Problem 18
+        "Conv. by Ratio Test",
+        
+        // Problem 19
+        "Conv. by AST",
+        
+        // Problem 20
+        "Div. by Nth Term Test"
+    };
     public TextMeshProUGUI[] questions;
     public WheelController wheelController;
 
     public GameObject stone;
 
     public TextMeshProUGUI fraction;
+    public TextMeshProUGUI answer;
+
+    public bool verge = false;
+    public bool isPopup = false;
+    public bool user_verge;
+    int randIndex;
     // Start is called before the first frame update
     void Start()
     {
@@ -85,6 +155,7 @@ public class WheelResult : MonoBehaviour
             ques.gameObject.SetActive(true);
         }
         stone.SetActive(false);
+        isPopup = false;
     }
 
     // Update is called once per frame
@@ -118,13 +189,64 @@ public class WheelResult : MonoBehaviour
 
     public void UIPopup()
     {
+        isPopup = true;
+        answer.gameObject.SetActive(false);
         foreach (TextMeshProUGUI ques in questions)
         {
             ques.gameObject.SetActive(false);
         }
         stone.SetActive(true);
         stone.transform.eulerAngles = new Vector3(0,0,0);
-        int randIndex = UnityEngine.Random.Range(0, expressions.Length);
+        randIndex = UnityEngine.Random.Range(0, expressions.Length);
         fraction.text = expressions[randIndex];
+        string type = answers[randIndex].Substring(0,3);
+        if (string.Equals(type, "Con"))
+        {
+            verge = true;
+        }
+        else
+        {
+            verge = false;
+        }
+
+    }
+
+    public void Converge()
+    {
+        if (isPopup)
+        {
+            user_verge = true;
+        }
+        if (verge != user_verge)
+        {
+            answer.gameObject.SetActive(true);
+            answer.text = "Wrong! " + answers[randIndex];
+        }
+        else
+        {
+            answer.gameObject.SetActive(true);
+            answer.text = "Correct!";
+        }
+    }
+    public void Diverge()
+    {
+        if (isPopup)
+        {
+            user_verge = false;
+        }
+        if (verge != user_verge)
+        {
+            answer.gameObject.SetActive(true);
+            answer.text = "Wrong! " + answers[randIndex];
+        }
+        else
+        {
+            answer.gameObject.SetActive(true);
+            answer.text = "Correct!";
+        }
+    }
+    public void resetPopup()
+    {
+        stone.SetActive(false);
     }
 }
